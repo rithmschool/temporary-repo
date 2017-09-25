536b55a1e2269fdd87a3a7ff3c4c56af36db8a6e
Matt Lane <lane.matthew@gmail.com>
2017-09-25 10:24:00 -0700

    updating numbers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   var num1 = 10;
   var num2 = 20;
-  var num3 = 30;
-  var sum = num1 + num2 + num3;
+  var num3 = 50;
+  var num4 = 100;
+  var sum = num1 + num2 + num3 + num4;
 });
